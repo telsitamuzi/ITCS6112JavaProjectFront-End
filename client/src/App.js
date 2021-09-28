ce4d0b6d43f2cc02a0271613f5ebd7a0b0eaa4ed
Telsi Tamuzi <ttamuzi@uncc.edu>
2021-09-28 16:28:03 -0400

    Created react application to connect to the backend

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import React, {useState, useEffect} from "react"; 
+import './App.css';
+import Axios from 'axios'
+
+function App() {
+
+  const [movieName, setMovieName] = useState("")
+  const [review, setReview] = useState("")
+  const [movieReviewList, setMovieList] = useState([])
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/api/get').then((response) => {
+      setMovieList(response.data)
+    })
+  }, []);
+
+  const submitReview = () =>{
+    Axios.post('http://localhost:3001/api/insert', {
+      movieName: movieName, 
+      movieReview: review,
+    }).then(() => {
+      alert("Successfule insert")
+    });
+  };
+
+  return(
+    <div className="App">
+      
+      <h1> Application</h1>
+
+      <div className= "form">
+        <label>Movie Name</label>
+        <input type="text" name="movieName" onChange={(e) => {
+          setMovieName(e.target.value)
+        
+      }}/>
+      <label>Review</label>
+        <input type="text" name="review" onChange={(e) => {
+          setReview(e.target.value)
+        
+      }}/>
+
+      <button onClick= {submitReview}>Submit</button>
+
+      {movieReviewList.map((val) =>{
+        return <h1> Movie NAme: {val.movieName} | Movie Review: (val.movieReview} </h1>
+      })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
