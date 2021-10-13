e923405d6fe3a305c650a662c41a04691a4668c0
Pasza <krispassa@gmail.com>
2021-10-13 17:28:18 -0400

    added signin

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import DemoComponent from './DemoComponent';
+import Signup from './Signup';
 
 function App() {
   return (
     <div className="App">
         <DemoComponent />
+        <Signup/>
     </div>
   );
 }
