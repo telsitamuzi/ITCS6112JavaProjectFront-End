5a619a38b4b796e7589d2e3efb97a9d73b6fb602
Pasza <krispassa@gmail.com>
2021-10-13 17:27:13 -0400

    signup component

---
+++ react-frontend/src/Signup.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Signup = () =>{
+
+    return(
+
+        <>
+        <div className="container">
+            <div className="app-wrapper">
+                <div>
+                    <h2 className="title">Create Account</h2>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Signup;
