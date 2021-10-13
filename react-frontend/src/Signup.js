9667404cea29fb6f4e22bf8bdee4879db2962274
Pasza <krispassa@gmail.com>
2021-10-13 18:34:08 -0400

    added signup component

--- react-frontend/src/Signup.js
+++ react-frontend/src/Signup.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 const Signup = () =>{
 
+    const[data, setData] = useState({
+        firstname:"",
+        lastname:"",
+        email:"",
+        password:"",
+    })
+    const handleFormSubmit = (e) =>{
+        e.preventDefault();
+    }
     return(
 
         <>
@@ -11,6 +20,30 @@ const Signup = () =>{
                 <div>
                     <h2 className="title">Create Account</h2>
                 </div>
+                <form className="form-wrapper">
+                    <div className="name">
+                        <label className="label">First Name</label>
+                        <input className="input" type="text" name="firstname" value={data.firstname}/>
+                    </div>
+                    <div className="name">
+                        <label className="label">Last Name</label>
+                        <input className="input" type="text" name="lastname" value={data.lastname}/>
+                    </div>
+                    <div className="email">
+                        <label className="label">Email</label>
+                        <input className="input" type="email" name="email" value={data.email}/>
+                    </div>
+                    <div className="password">
+                        <label className="label">Password</label>
+                        <input className="input" type="email" name="password" value={data.password}/>
+                    </div>
+                    <div>
+                        <button className="submit" onClick={handleFormSubmit}>
+                            Sign Up
+                        </button>
+                        { JSON.stringify(data) }
+                    </div>
+                </form>
             </div>
         </div>
         </>
