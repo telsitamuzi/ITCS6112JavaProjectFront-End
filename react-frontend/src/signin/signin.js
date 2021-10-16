b5b50f54c678a5b8c046149f7b9806fd02698c08
krsna <krispassa@gmail.com>
2021-10-16 12:21:27 -0400

    buid fix

--- react-frontend/src/signin/signin.js
+++ react-frontend/src/signin/signin.js
@@ -25,7 +25,7 @@ export default class SignIn extends Component {
 
                 <button type="submit" className="btn btn-primary btn-block">Submit</button>
                 <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
+                    Forgot <a href="/">password?</a>
                 </p>
             </form>
         );
